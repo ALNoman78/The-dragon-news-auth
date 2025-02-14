0fad2665085f33d67e1498a625224d3e77fd4bf5
ALNoman78 <abdullaalno078@gmail.com>
2025-02-14 14:32:27 +0600

    crate router and auth provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createContext } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+function App({ children }) {
+
+  export const AuthContext = createContext(null)
+  const name = 'abdullah al noman'
+
+  const authInfo = {
+    name,
+  }
 
   return (
     <>
+      <AuthContext.Provider value={authInfo}>
+        {children}
+      </AuthContext.Provider>
     </>
   )
 }
---
+++ src/AuthProvider/PrivateAuth.jsx
@@ -0,0 +1,9 @@
+const PrivateAuth = () => {
+    return (
+        <div>PrivateAuth</div>
+    )
+}
+
+PrivateAuth.propTypes = {}
+
+export default PrivateAuth
