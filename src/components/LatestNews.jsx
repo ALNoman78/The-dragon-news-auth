5c423731c1f597ac082effc25c50fdab620aa9ba
ALNoman78 <abdullaalno078@gmail.com>
2025-02-23 22:42:28 +0600

    latest news section update

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 const LatestNews = () => {
     return (
         <div className="bg-base-200 p-4 gap-5 flex items-center">
-            <p className="px-5 py-1 font-medium bg-pink-500 text-lg">Latest</p>
+            <p className="px-5 py-1 font-medium bg-pink-500 text-lg text-white">Latest</p>
             <Marquee pauseOnHover={true}  className="space-x-10">
                 <Link to='news'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore magnam dolores laborum sunt libero officia.</Link>
                 <Link to='news'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore magnam dolores laborum sunt libero officia.</Link>
