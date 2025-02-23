c4b13ac1d629a7d851ed9ad9290fd8c1bf8db231
ALNoman78 <abdullaalno078@gmail.com>
2025-02-23 20:09:49 +0600

    main content section layout

--- src/Layouts/Home.jsx
+++ src/Layouts/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header"
 import LatestNews from "../components/LatestNews"
+import Navbar from "../components/Navbar"
 
 const Home = () => {
     return (
@@ -10,9 +11,14 @@ const Home = () => {
                     <LatestNews></LatestNews>
                 </section>
             </header>
-            <nav>
-
+            <nav className="md:w-11/12 mx-auto">
+                <Navbar></Navbar>
             </nav>
+            <main className="md:w-11/12 mx-auto pt-6 md:grid grid-cols-12 gap-3">
+                <aside className="left col-span-3">left content</aside>
+                <section className="col-span-6">Main Content</section>
+                <aside className="col-span-3">Right content</aside>
+            </main>
         </div>
     )
 }
